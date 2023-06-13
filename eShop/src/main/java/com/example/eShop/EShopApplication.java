4998c362e38737bb1ed7c7c58ecfeeb42e0ba7b7
Arteni Teodor <arteni.teodor@gmail.com>
2023-06-13 22:38:40 +0300

    test

--- eShop/src/main/java/com/example/eShop/EShopApplication.java
+++ eShop/src/main/java/com/example/eShop/EShopApplication.java
@@ -9,5 +9,6 @@ import java.sql.SQLException;
 public class EShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(EShopApplication.class, args);
+        //test
     }
 }
